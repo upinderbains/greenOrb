f6c56157e6e281c85151e87a7558ffc9ed31aa20
Upinder Bains <bains@Upinders-MacBook-Pro.local>
2020-08-14 21:02:38 -0400

    fix url issue -6

--- db/connection.js
+++ db/connection.js
@@ -3,14 +3,11 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      'mongodb+srv://pinder_twitter:nWKscGso7P1PBFrg@cluster0-mbkuh.mongodb.net/greenOrb?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
     console.log('MongoDB connected');
   } catch (error) {
     console.log(error.message);
